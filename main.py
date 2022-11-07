f543e2c6c58af93088300010614ceec07d74767e
Richie <liuruichao@gmail.com>
2022-11-07 20:17:13 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 blocks.place(REDSTONE, pos(0, 0, 0))
 blocks.place(GRASS, pos(0, 0, 0))
 player.say("github")
+blocks.test_for_block(GRASS, pos(0, 0, 0))
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 blocks.place(REDSTONE, pos(0, 0, 0))
 blocks.place(GRASS, pos(0, 0, 0))
 player.say("github")
+blocks.testForBlock(GRASS, pos(0, 0, 0))
